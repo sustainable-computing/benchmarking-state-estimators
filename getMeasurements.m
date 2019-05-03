3f990d2ae832f26019ec61f573ea8fe64c8e7bd5
Moosa Moghimi <Moosa Moghimi@10.13.14.188>
2019-05-02 18:29:10 -0600

    Include PMU current readings in the getMeasurement file

--- getMeasurements.m
+++ getMeasurements.m
@@ -1,4 +1,4 @@
-function [z,zType]=getMeasurements(Vtrue, ~, loadPower, PMUnodes)
+function [z,zType]=getMeasurements(Vnoisy, Inoisy, loadPower, PMUnodes)
 %measurements
 %Load power
 z=loadPower(:,2);%real powers of the nodes
@@ -9,7 +9,7 @@ z=[z;loadPower(:,3)];%reactive powers of the nodes
 zTypeAdd(:,2)=loadPower(:,1);%defining the corresponding nodes 
 zTypeAdd(:,1)=4;%defining measurement type
 zType=[zType;zTypeAdd];
-zType(:,3)=0;
+zType(:,3:4)=0;
 z=-z;%injected powers
 
 %Adding 1st node with zero load
@@ -19,16 +19,38 @@ z=-z;%injected powers
 
 %voltage magnitudes
 PMUnodes1ph=sort([3*PMUnodes-2,3*PMUnodes-1,3*PMUnodes]);
-z=[z;abs(Vtrue(PMUnodes1ph))'];
+z=[z;abs(Vnoisy(PMUnodes1ph))'];
 zTypeAdd=5*ones(length(PMUnodes1ph),1);%type 5 is voltage magnitude
 zTypeAdd(:,2)=PMUnodes1ph';
-zTypeAdd(:,3)=0;
+zTypeAdd(:,3:4)=0;
 zType=[zType;zTypeAdd];
 
 %voltage angles
-z=[z;(angle(Vtrue(PMUnodes1ph(2:end)))-angle(Vtrue(1)))'];
+z=[z;(angle(Vnoisy(PMUnodes1ph(2:end)))-angle(Vnoisy(1)))'];
 zTypeAdd=6*ones(length(PMUnodes1ph)-1,1);%type 6 is voltage angle
 zTypeAdd(:,2)=(PMUnodes1ph(2:end))';
-zTypeAdd(:,3)=0;
+zTypeAdd(:,3:4)=0;
 zType=[zType;zTypeAdd];
+
+%line current real part
+PMUCurrentMap=[1,2:2:64];%PMU is installed downstream of node 1 and upstream of all the other nodes
+for pm=1:length(PMUnodes)
+    pmu=PMUnodes(pm);
+    z=[z;Inoisy(PMUCurrentMap(pmu),3:2:7).'];
+    zTypeAdd=7*ones(3,1);%each PMU gives 3 real current type7
+    zTypeAdd(:,2:3)=repmat(Inoisy(PMUCurrentMap(pmu),1:2),3,1);
+    zTypeAdd(:,4)=[1;2;3];%phase number
+    zType=[zType;zTypeAdd];
+end
+
+%line current imaginary part
+for pm=1:length(PMUnodes)
+    pmu=PMUnodes(pm);
+    z=[z;Inoisy(PMUCurrentMap(pmu),4:2:8).'];
+    zTypeAdd=8*ones(3,1);%each PMU gives 3 imaginary current type8
+    zTypeAdd(:,2:3)=repmat(Inoisy(PMUCurrentMap(pmu),1:2),3,1);
+    zTypeAdd(:,4)=[1;2;3];%phase number
+    zType=[zType;zTypeAdd];
+end
+
 end

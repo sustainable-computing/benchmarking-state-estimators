b57f8e8cbcb1f2b6e71b09bf07ddad920adfd437
Moongar <moosa.moghimi@gmail.com>
2019-07-10 15:30:57 -0600

    Update to handle voltage and current measurements or just voltage measurement

--- mainEnKF.m
+++ mainEnKF.m
@@ -13,6 +13,12 @@ if ~flag
     error('Fatal error!\n Failed to create the COM object to interface with OpenDSS');
 end
 
+% DSSText=DSSObj.text;
+% DSSCircuit=DSSObj.ActiveCircuit;
+% DSSText.Command='Set mode=snapshot';
+% DSSText.Command='solve';
+% DSSText.command='show voltages';
+
 %% Getting Ybus matrix
 %manual Ybus
 Y=manualYMatrix33ph;
@@ -24,13 +30,27 @@ Qref=[60;40;80;30;20;100;100;20;20;30;35;35;80;10;20;20;40;40;40;40;40;50;200;20
 
 secNodes=55;%number of load nodes in the secondary power system
 dur=3600;% the pseudo meas. will be available for every dur seconds
-resol=60;%data resolution in sec. we take a PMU reading every resol seconds
+resol=1;%data resolution in sec. we take a PMU reading every resol seconds
 %Assigne load curves to nodes (using ADRES data set)
 %getting the required load indices for connecting houses to secondary nodes
 %and getting predicted loads for the next day
-delay=0;%number of semples that PMU data is taking to be processed and communicated to us
-% [P,Q,delayedP,delayedQ,PpMean,PpStd,QpMean,QpStd,PdiffStd,QdiffStd,Ppsi,Qpsi]=loadProcessAssignHalf1ph(Pref,secNodes,dur,resol,delay);
-load('LoadDataFactor2Delay0Resol60Dur3600')
+delay=1;%number of semples that PMU data is taking to be processed and communicated to us
+currentPhasor = false;%if you want to consider the current phasor readings from PMU
+factor=1;
+% [P,Q,delayedP,delayedQ,PpMean,PpStd,QpMean,QpStd,PdiffStd,QdiffStd,Ppsi,Qpsi]=loadProcessAssignHalf1ph(Pref,secNodes,dur,resol,delay,factor);
+% [P,Q]=loadProcessAssign1ph1Week(Pref,secNodes,dur,resol,delay,factor);
+moosa = 1;
+load('newLoadF10D1R60Dur3600')
+if delay == 0
+    delayedP = P;
+    delayedQ = Q;
+end
+% PpMean=PpMean/3;
+% PpStd=PpStd/3;
+% PdiffStd=PdiffStd/3;
+% QpMean=QpMean/3;
+% QpStd=QpStd/3;
+% QdiffStd=QdiffStd/3;
 
 %% perunit values
 sBase=100e3/3; %single phase power base
@@ -63,10 +83,13 @@ MSE_EKF=zeros(1,length(P(1,:)));
 %PMUs at all nodes
 MSE_PMU=zeros(1,length(P(1,:)));
 
+%loading loadflow results
+% load('LoadFlowResultsR60.mat');
+
 %% main loop
-for k=1:10%length(P(1,:))
+for k=1:100%length(P(1,:))
     disp("hour: "+ k);
-    %% setting up OpenDSS
+%     %% setting up OpenDSS
     text = pwd;%current path
     dss_path = [text, '\master33Full.dss'];
     [DSSObj, flag] = DSSStartup(dss_path);
@@ -88,35 +111,53 @@ for k=1:10%length(P(1,:))
     
     %% converting to per-unit
     Vtrue(1:priNodes)=Vtrue(1:priNodes)/vBasePri;
-%     Vtrue(priNodes+1:end)=Vtrue(priNodes+1:end)/vBaseSec*sqrt(3);
+    Vtrue(priNodes+1:end)=Vtrue(priNodes+1:end)/vBaseSec*sqrt(3);
 %     linePowerTransfer(:,[3,4])=linePowerTransfer(:,[3,4])/sBase;
     PseudoLoadPower(:,[2,3])=PseudoLoadPower(:,[2,3])/sBase*1000;%PseudoLoadPower is in kW
     VtruePrim=Vtrue(1:priNodes);%getting the primary nodes voltages
     disp("initial error: "+ sqrt(1/99*sum(abs(VtruePrim-repmat([1,exp(-2*pi*1i/3),exp(-4*pi*1i/3)],1,33)).^2)))
     VdelayedPrim=Vdelayed(1:priNodes)/vBasePri;
-    lineCurrentDelayed(:,3:8)=lineCurrentDelayed(:,3:8)/Ibase;
+    
     
     %% adding noise
     sigmaPMU=.01*.05/3;%micro-PMU
-%     rdnV=sigmaPMU*randn(1,length(VtruePrim));%adding noise to magnitude only
-    rdnV=sigmaPMU*randn(1,length(VtruePrim));
+    rdnV=sigmaPMU*randn(1,length(VtruePrim));%adding noise to magnitude only
     for rr=1:length(rdnV)
         alpha=rand*2*pi;
         rdnV(rr)=rdnV(rr)*(cos(alpha)+1i*sin(alpha));
     end
     % calculate the max angle difference
     Vnoisy=VdelayedPrim+rdnV;%adding total vector error
-%     VnoisyAngleDiff=max(angle(Vnoisy)-angle(VdelayedPrim));
-    %adding noise to current readings
-    rdnI=sigmaPMU/sqrt(2)*randn(2*32,6);
-    lineCurrentNoisy=lineCurrentDelayed(:,1:2);
-    lineCurrentNoisy(:,3:8)=0;
-    lineCurrentNoisy(:,3:8)=lineCurrentDelayed(:,3:8).*(1+rdnI);
+    VnoisyAngleDiff=max(angle(Vnoisy)-angle(VdelayedPrim));
+    if currentPhasor == true
+        %per unit
+        lineCurrentDelayed(:,3:8)=lineCurrentDelayed(:,3:8)/Ibase;
+        %adding noise to current readings
+        rdnI=sigmaPMU/sqrt(2)*randn(2*32,6);
+        lineCurrentNoisy=lineCurrentDelayed(:,1:2);
+        lineCurrentNoisy(:,3:8)=0;
+        lineCurrentNoisy(:,3:8)=lineCurrentDelayed(:,3:8).*(1+rdnI);
+    else
+        lineCurrentNoisy = [];
+    end
+%     %load flow results
+%     Vnoisy=AllVnoisy(k,:);
+%     lineCurrentNoisy=AllLineCurrentNoisy(:,:,k);
+%     if delay==1
+%         VtruePrim=AllVtruePrim(k,:);
+%     else
+%         VtruePrim=AllVdelayedPrim(k,:);
+%     end
 
     %covariance matrix of the error
-    R=diag([PpStd(:,ceil(k*resol/dur)).'.^2,QpStd(:,ceil(k*resol/dur)).'.^2,...%Pseudo measurements
-        sigmaPMU^2*ones(1,3*2*length(PMUnodes)-1),...%PMU voltage measurement
-        sigmaPMU^2*ones(1,3*2*length(PMUnodes))]);%PMU current measurement
+    if currentPhasor == true
+        R=diag([PpStd(:,ceil(k*resol/dur)).'.^2,QpStd(:,ceil(k*resol/dur)).'.^2,...%Pseudo measurements
+            sigmaPMU^2*ones(1,3*2*length(PMUnodes)-1),...%PMU voltage measurement
+            sigmaPMU^2*ones(1,3*2*length(PMUnodes))]);%PMU current measurement
+    else
+        R=diag([PpStd(:,ceil(k*resol/dur)).'.^2,QpStd(:,ceil(k*resol/dur)).'.^2,...%Pseudo measurements
+            sigmaPMU^2*ones(1,3*2*length(PMUnodes)-1)]);
+    end
     
     %error if we had PMUs at all nodes
     MSE_PMU(k)=(1/99*sum(abs(VtruePrim-Vnoisy).^2));
@@ -124,7 +165,7 @@ for k=1:10%length(P(1,:))
     
     %% Defining measurement data for WLS SE
     % [z,zType]=getMeasurements(Vtrue, linePowerTransfer, PseudoLoadPower);%for testing the accuracy of WLS method without any noise
-    [z,zType]=getMeasurements(Vnoisy, lineCurrentNoisy, PseudoLoadPower, PMUnodes);%WLS with noisy data
+    [z,zType]=getMeasurements(Vnoisy, lineCurrentNoisy, PseudoLoadPower, PMUnodes, currentPhasor);%WLS with noisy data
     
     %% performing WLS SE
     iter_max=10;
@@ -220,19 +261,21 @@ for k=1:10%length(P(1,:))
     %Calculating Xa at iteration k
     Xa=Xu+K*(Z-H_hat*X_hat);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %PMU Current phasor assimilation
-    PMUCurrentMap=[1,2:2:64];
-    PMU_Iidx=PMUCurrentMap(PMUnodes);
-    ZI_PMU=[lineCurrentNoisy(PMU_Iidx,3:2:7);lineCurrentNoisy(PMU_Iidx,4:2:8)];
-    ZI_PMU=reshape(ZI_PMU.',[],1);
-    Z=ZI_PMU.*(1+sigmaPMU/sqrt(2)*randn(length(ZI_PMU),L));
-    % now we need to do a load flow for each ensemble of Xa
-    for l=1:L
-        %load flow
-        [Vxl,Ixl]=runPF(DSSObj,Xa(1:length(PpMean(:,1)),l),Xa(length(PpMean(:,1))+1:end,l));
-        Ixl(:,3:8)=Ixl(:,3:8)/Ibase;
-        Ixl_PMU=[Ixl(PMU_Iidx,3:2:7);Ixl(PMU_Iidx,4:2:8)];
-        hX(:,l)=reshape(Ixl_PMU.',[],1);
+    if currentPhasor == true
+        %PMU Current phasor assimilation
+        PMUCurrentMap=[1,2:2:64];
+        PMU_Iidx=PMUCurrentMap(PMUnodes);
+        ZI_PMU=[lineCurrentNoisy(PMU_Iidx,3:2:7);lineCurrentNoisy(PMU_Iidx,4:2:8)];
+        ZI_PMU=reshape(ZI_PMU.',[],1);
+        Z=ZI_PMU.*(1+sigmaPMU/sqrt(2)*randn(length(ZI_PMU),L));
+        % now we need to do a load flow for each ensemble of Xa
+        for l=1:L
+            %load flow
+            [Vxl,Ixl]=runPF(DSSObj,Xa(1:length(PpMean(:,1)),l),Xa(length(PpMean(:,1))+1:end,l));
+            Ixl(:,3:8)=Ixl(:,3:8)/Ibase;
+            Ixl_PMU=[Ixl(PMU_Iidx,3:2:7);Ixl(PMU_Iidx,4:2:8)];
+            hX(:,l)=reshape(Ixl_PMU.',[],1);
+        end
     end
     %Augmented ensemble X_hat
     X_hat=[Xa;hX];
